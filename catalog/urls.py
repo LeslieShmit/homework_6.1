afede78617d438e111adf7377774d87a30583774
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-03 23:58:24 +0400

    update settings.py and urls.py for working with media

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from catalog.apps import CatalogConfig
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
