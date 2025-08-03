69c69c0ab0f4bc6ea1bb6b7457b8841f5b636302
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-04 00:00:35 +0400

    fixes

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование категории')
-    description = models.TextField(verbose_name='Описание', null=True, blank=True, help_text='Введите описание категории')
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование категории', help_text='Введите наименование категории')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True,
+                                   help_text='Введите описание категории')
 
     def __str__(self):
         return self.name
@@ -13,15 +14,17 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
         ordering = ['name',]
 
+
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование товара')
+    name = models.CharField(max_length=150, verbose_name='Наименование товара', help_text='Введите наименование товара')
     description = models.TextField(verbose_name='Описание', null=True, blank=True, help_text='Введите описание товара')
-    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Изображение', help_text='Загрузите изображение товара')
+    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Изображение',
+                              help_text='Загрузите изображение товара')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Стоимость', help_text='Введите стоимость товара')
+    price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Стоимость',
+                                help_text='Введите стоимость товара')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата последнего изменения')
-
 
     def __str__(self):
         return self.name
