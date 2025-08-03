efd981feb0b7bd322a77f11ff161251255e3e89a
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-03 21:57:56 +0400

    add models Product, Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование категории')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True, help_text='Введите описание категории')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name',]
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование товара')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True, help_text='Введите описание товара')
+    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Изображение', help_text='Загрузите изображение товара')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Стоимость', help_text='Введите стоимость товара')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ['name',]
