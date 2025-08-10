e9518543d076da6f8b94f52cc3d2136a841649fb
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-10 22:09:49 +0400

    update urls.py and views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 from catalog.apps import CatalogConfig
 
-
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('product_details/<int:pk>', views.product_details, name='product_details')
 ]
