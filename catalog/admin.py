f6c31c631dbda34805de54ee2d2a265636712293
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-17 11:29:41 +0400

    registered model Contact in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Contact
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,3 +12,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category__name',)
     list_filter = ('category__name',)
     search_fields = ('name', 'description',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'country', 'tin', 'address',)
+    list_filter = ('country',)
+    search_fields = ('country', 'tin', 'address',)
