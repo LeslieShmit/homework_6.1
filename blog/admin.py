9fd9873ce2e4c7fce90f833528ea35709ca618e4
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-17 17:40:42 +0400

    registered Blog model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'is_published', 'views_counter')
+    list_filter = ('is_published',)
+    search_fields = ('title', 'created_at')
