0785897af0477cf9e45f09cb0825f4a91f38a7c8
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-17 17:06:22 +0400

    fixes for Blog model

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,6 @@ class Blog(models.Model):
     content = models.TextField(verbose_name='Содержимое', null=True, blank=True, help_text='Тут должен быть текст вашей статьи')
     image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Превью',
                               help_text='Загрузите изображение для превью')
-    price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Стоимость',
-                                help_text='Введите стоимость товара')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=True, verbose_name='Признак публикации', help_text='Укажите, была ли статья опубликована')
     views_counter = models.PositiveIntegerField(verbose_name='Счетчик просмотров')
