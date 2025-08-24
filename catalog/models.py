0689697c0b805e4a9fc10e693865c550be4727f1
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-08-24 17:52:35 +0400

    add is_available field to Product model

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='Стоимость',
                                 help_text='Введите стоимость товара')
+    is_available = models.BooleanField(default=True, verbose_name='Наличие товара', help_text='Укажите, есть ли товар в наличии')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
