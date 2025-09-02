59afbc2b678687bd6a129bef57db4765665432a5
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-09-02 18:57:43 +0400

    add LoginRequierdMixins

--- config/settings.py
+++ config/settings.py
@@ -149,4 +149,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT_URL = reverse_lazy('catalog:home')
+LOGIN_REDIRECT_URL = 'catalog:home'
+LOGIN_URL = 'users:login'
