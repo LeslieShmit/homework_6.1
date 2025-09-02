c59f5fa486b67d4e0cb94350bd7aa3c3f2bd54d0
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-09-02 18:50:53 +0400

    add urls and update settings

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from django.urls import reverse_lazy
 
 load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,3 +148,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = reverse_lazy('catalog:home')
