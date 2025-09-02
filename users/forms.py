f417976647bc5a91205d1a7b0c664f96d4a389f1
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-09-02 18:49:44 +0400

    add register form

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(required=False, max_length=15, help_text='Введите номер телефона (опционально)')
+    country = forms.CharField(required=False, max_length=50)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'country', 'phone_number', 'password1', 'password2')
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if phone_number and not phone_number.isdigit():
+            raise forms.ValidationError('Номер телефона может состоять только из цифр')
+        return phone_number
